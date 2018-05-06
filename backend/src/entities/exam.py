1f5295d922bc5ba32ed0deba2f4f25655a47a43a
Sunil <sunil9766@gmail.com>
2018-05-06 15:13:12 +0530

    ExamSchema add

--- backend/src/entities/exam.py
+++ backend/src/entities/exam.py
@@ -1,5 +1,5 @@
+from marshmallow import Schema, fields
 from sqlalchemy import Column, String
-
 from .entity import Entity, Base
 
 
@@ -13,3 +13,12 @@ class Exam(Entity, Base):
         Entity.__init__(self, created_by)
         self.title = title
         self.description = description
+
+
+class ExamSchema(Schema):
+    id = fields.Number()
+    title = fields.Str()
+    description = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
