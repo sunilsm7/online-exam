33602be7ae3ce4f62cdeaefd721e64b0145037a1
Sunil <sunil9766@gmail.com>
2018-05-25 12:25:20 +0530

    added examslist and examform component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,27 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {ExamsApiService} from './exams/exams-api.service';
 
+import {ExamFormComponent} from './exams/exam-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExamsComponent} from './exams/exams.component';
+
+const appRoutes: Routes = [
+	{path: 'new-exam', component: ExamFormComponent },
+	{path: '', component: ExamsComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+    	appRoutes,
+    )
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
--- frontend/src/app/exams/exams-api.service.ts
+++ frontend/src/app/exams/exams-api.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { API_URL } from '../env';
-import { Exam } from './exam.model';
-
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {catchError} from 'rxjs/operators'
+import {API_URL} from '../env';
+import {Exam} from './exam.model';
 
 @Injectable()
 export class ExamsApiService {
@@ -13,15 +12,20 @@ export class ExamsApiService {
   }
 
   private static _handleError(err: HttpErrorResponse | any) {
-    return Observable.throw(err.message || 'Error: Unable to complete request.');    
+    return Observable.throw(err.message || 'Error: Unable to complete request.');
   }
 
   // GET list of public, future events
-  getExams(): Observable<Exam[]> {
+   getExams(): Observable<Exam[]> {
     return this.http
-    .get(`${API_URL}/exams`)
-    // .catchError(ExamsApiService._handleError);
-    catchError(err => ExamsApiService._handleError)
-
+      .get(`${API_URL}/exams`)
+      catchError(ExamsApiService._handleError);
   }
+
+  // Save Exams
+  saveExam(exam: Exam): Observable<any> {
+    return this.http
+      .post(`${API_URL}/exams`, exam)
+  }
+  
 }
